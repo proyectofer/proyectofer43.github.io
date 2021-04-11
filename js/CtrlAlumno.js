c5e31bd9aabfddaa419f51055861b603314e56d4
proyectofer <82188609+proyectofer@users.noreply.github.com>
2021-04-11 14:06:59 -0500

    Update CtrlAlumno.js

--- js/CtrlAlumno.js
+++ js/CtrlAlumno.js
@@ -3,28 +3,23 @@ import {
   getFirestore
 } from "../lib/fabrica.js";
 import {
-  getString,
+  cod,
   muestraError
 } from "../lib/util.js";
-import {
-  muestraAlumnos
-} from "./navegacion.js";
 import {
   tieneRol
 } from "./seguridad.js";
 
+/** @type {HTMLUListElement} */
+const lista = document.
+  querySelector("#lista");
 const daoAlumno =
   getFirestore().
     collection("Alumno");
-const params =
-  new URL(location.href).
-    searchParams;
-const id = params.get("id");
-/** @type {HTMLFormElement} */
-const forma = document["forma"];
 
-getAuth().onAuthStateChanged(
-  protege, muestraError);
+getAuth().
+  onAuthStateChanged(
+    protege, muestraError);
 
 /** @param {import(
     "../lib/tiposFire.js").User}
@@ -32,79 +27,68 @@ getAuth().onAuthStateChanged(
 async function protege(usuario) {
   if (tieneRol(usuario,
     ["Administrador"])) {
-    busca();
+    consulta();
   }
 }
 
-/** Busca y muestra los datos que
- * corresponden al id recibido. */
-async function busca() {
-  try {
-    const doc =
-      await daoAlumno.
-        doc(id).
-        get();
-    if (doc.exists) {
-      /**
-       * @type {
-          import("./tipos.js").
+function consulta() {
+  daoAlumno.
+    orderBy("nombre")
+    .onSnapshot(
+      htmlLista, errConsulta);
+}
+
+/**
+ * @param {import(
+    "../lib/tiposFire.js").
+    QuerySnapshot} snap */
+function htmlLista(snap) {
+  let html = "";
+  if (snap.size > 0) {
+    snap.forEach(doc =>
+      html += htmlFila(doc));
+  } else {
+    html += /* html */
+      `<li class="vacio">
+        -- No hay alumnos
+        registrados. --
+      </li>`;
+  }
+  lista.innerHTML = html;
+}
+
+/**
+ * @param {import(
+    "../lib/tiposFire.js").
+    DocumentSnapshot} doc */
+function htmlFila(doc) {
+  /**
+   * @type {import("./tipos.js").
                   Alumno} */
-      const data = doc.data();
-      forma.matricula.value = data.matricula;
-      forma.nombre.value =
-        data.nombre || "";
-      forma.addEventListener(
-        "submit", guarda);
-      forma.eliminar.
-        addEventListener(
-          "click", elimina);
-    } else {
-      throw new Error(
-        "No se encontró.");
-    }
-  } catch (e) {
-    muestraError(e);
-    muestraAlumnos();
-  }
+  const data = doc.data();
+  const matricula = cod(data.matricula);
+  const nombre = cod(data.nombre);
+  var fsf= cod(data.fecha);
+  var fecha = new Date(fsf);
+  var espacio="[   -   ]";
+  var dformat = [fecha.getDate()+1, fecha.getMonth()+1, fecha.getFullYear()].join('/');
+  const parámetros =
+    new URLSearchParams();
+  parámetros.append("id", doc.id);
+  return ( /* html */
+    `<li>
+      <a class="fila" href=
+  "alumno.html?${parámetros}">
+        <strong class="primario">
+          ${matricula} ${nombre} ${dformat}
+        </strong>
+      </a>
+     
+    </li>`);
 }
 
-/** @param {Event} evt */
-async function guarda(evt) {
-  try {
-    evt.preventDefault();
-    const formData =
-      new FormData(forma);
-    const matricula = getString(
-        formData, "matricula").trim();  
-    const nombre = getString(
-      formData, "nombre").trim();
-    /**
-     * @type {
-        import("./tipos.js").
-                Alumno} */
-    const modelo = {
-      matricula, 
-      nombre
-    };
-    await daoAlumno.
-      doc(id).
-      set(modelo);
-    muestraAlumnos();
-  } catch (e) {
-    muestraError(e);
-  }
-}
-
-async function elimina() {
-  try {
-    if (confirm("Confirmar la " +
-      "eliminación")) {
-      await daoAlumno.
-        doc(id).
-        delete();
-      muestraAlumnos();
-    }
-  } catch (e) {
-    muestraError(e);
-  }
+/** @param {Error} e */
+function errConsulta(e) {
+  muestraError(e);
+  consulta();
 }
